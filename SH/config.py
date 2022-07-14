254fed7d12e17bdf6c4c7cd4dd8d873db2b23d85
sherry.jeon <sherry.jeon@brique.co.kr>
2022-07-14 13:26:02 +0900

    config 추가

---
+++ SH/config.py
@@ -0,0 +1,7 @@
+from types import SimpleNamespace
+
+config_elem = {
+    'feats_store_dir': 'G:/공유 드라이브/BRIQUE/(B)과제/(BB)내부/기타/BI-Y20-DS-005-1 TraceDataAnalysis/03.분석/feature_store'
+}
+
+conf = SimpleNamespace(**config_elem)
